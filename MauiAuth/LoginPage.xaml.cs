3b7990bf9fa97ff0acf7516fb4d09927d684803b
jobkarani <jobkarani084j@gmail.com>
2022-10-05 16:31:36 +0300

    Routing

--- MauiAuth/LoginPage.xaml.cs
+++ MauiAuth/LoginPage.xaml.cs
@@ -9,11 +9,11 @@ public partial class LoginPage : ContentPage
 	}
 	private async void Button_Clicked(object sender, EventArgs e)
 	{
-
+		await Shell.Current.GoToAsync("MainPage");
 	}
 
 	private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
 	{
-
-	}
+        await Shell.Current.GoToAsync("RegistrationPage");
+    }
 }
