01c253e48606d1dc3a10f9970355c24cc858d66d
jobkarani <jobkarani084j@gmail.com>
2022-10-07 14:20:35 +0300

    Registration page

--- MauiAuth/RegistrationPage.xaml.cs
+++ MauiAuth/RegistrationPage.xaml.cs
@@ -1,9 +1,18 @@
 namespace MauiAuth;
 
+
 public partial class RegistrationPage : ContentPage
 {
 	public RegistrationPage()
 	{
 		InitializeComponent();
 	}
+    private async void Buttonn_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("MainPage");
+    }
+    public async void BackToLogin(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("LoginPage");
+    }
 }
