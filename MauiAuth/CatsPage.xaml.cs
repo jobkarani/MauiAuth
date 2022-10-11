cd88cedbd67ccc06621f8779f2aedca1091b1c2f
jobkarani <jobkarani084j@gmail.com>
2022-10-11 08:58:10 +0300

    styles registration page

---
+++ MauiAuth/CatsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAuth;
+
+public partial class CatsPage : ContentPage
+{
+	public CatsPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAuth/DogsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAuth;
+
+public partial class DogsPage : ContentPage
+{
+	public DogsPage()
+	{
+		InitializeComponent();
+	}
+}
